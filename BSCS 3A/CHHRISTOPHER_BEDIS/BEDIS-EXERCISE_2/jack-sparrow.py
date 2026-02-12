3300a791aa51efffe72dd28d739f5a17e928663f
Christopher-Glenn <christopherglenn.bedis@wvsu.edu.ph>
2026-02-12 14:17:33 +0800

    fix(file-structure): transferred exercise 2 files in a folder and submitted exercise 4.1

--- BSCS 3A/CHHRISTOPHER_BEDIS/BEDIS-EXERCISE_2/jack-sparrow.py
+++ BSCS 3A/CHHRISTOPHER_BEDIS/BEDIS-EXERCISE_2/jack-sparrow.py
@@ -5,7 +5,6 @@ text = webtext.raw('pirates.txt')
 
 pattern = r'^\s*JACK SPARROW:.*$'
 
-# Extract all lines
 jack_lines = re.findall(pattern, text, re.MULTILINE | re.IGNORECASE)
 
 print(f"Total Jack Sparrow lines: {len(jack_lines)}")
